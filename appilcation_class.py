55097525ebf0ed7bafb46d51c446f6afc0f8db8a
PomergranateFan <dmitry1680490@gmail.com>
2023-04-06 17:04:21 +0300

    Add new methods to class

--- appilcation_class.py
+++ appilcation_class.py
@@ -40,4 +40,36 @@ class Application(tk.Frame):
         self.decrypt_button.grid(row=4, column=2, padx=5, pady=5, sticky=tk.W)
         self.quit_button = tk.Button(self, text="Quit", fg="red", command=self.master.destroy)
         self.quit_button.grid(row=5, column=2, padx=5, pady=5)
+    def browse_input_file(self):
+        """Open file dialog to select input file"""
+        self.input_file_path = filedialog.askopenfilename()
+        self.input_path_label.config(text=self.input_file_path)
+
+ 
+
+
+    def browse_output_file(self):
+        """Open file dialog to select output file"""
+        self.output_file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+        self.output_path_label.config(text=self.output_file_path)
+
+    def encrypt(self):
+        """Encrypt input file and save to output file"""
+        cipher_text = ""
+        with open(self.input_file_path, "r") as f:
+            plain_text = f.read().strip()
+        key = self.key_entry.get()
+        cipher_type = self.cipher_variable.get()
+
+        if cipher_type == "Caesar":
+            cipher_text = caesar_encrypt(plain_text, int(key))
+        elif cipher_type == "Vigenere":
+            cipher_text = vigenere_encrypt(plain_text, key)
+        elif cipher_type == "Vernam":
+            cipher_text = vernam_encrypt(plain_text, key)
+
+        with open(self.output_file_path, "w") as f:
+            f.write(cipher_text)
+        messagebox.showinfo("Encryption", "Encryption successful!")
+
 
