2909473f46bf23c533645f1def120a380e61c254
PomergranateFan <dmitry1680490@gmail.com>
2023-04-06 15:05:04 +0300

    Change functions of decrypt and encrypt for verman

--- vernam_decrypt.py
+++ vernam_decrypt.py
@@ -1,6 +1,9 @@
-
 def vernam_decrypt(cipher_text, key):
     """Decrypt ciphertext encrypted with Vernam cipher using the given key"""
     plain_text = ""
     for i in range(len(cipher_text)):
-        shifted = ch
+        shifted = chr(ord(cipher_text[i]) ^ ord(key[i % len(key)]))
+        plain_text += shifted
+    return plain_text
+
+
