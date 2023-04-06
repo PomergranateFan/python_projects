5c876a64b4c5df45e27ce1e5cb5a181af4c18702
PomergranateFan <dmitry1680490@gmail.com>
2023-04-06 14:53:37 +0300

    Add files with functions vernam encrypt and decrypt

---
+++ vernam_decrypt.py
@@ -0,0 +1,6 @@
+
+def vernam_decrypt(cipher_text, key):
+    """Decrypt ciphertext encrypted with Vernam cipher using the given key"""
+    plain_text = ""
+    for i in range(len(cipher_text)):
+        shifted = ch
